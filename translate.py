0181c7ede39e9d3f5673d295fdc4b03101d3db16
Kelvin-T-Lu <61221557+Kelvin-T-Lu@users.noreply.github.com>
2023-10-13 16:16:42 -0400

    mock translate w/o apply

--- translate.py
+++ translate.py
@@ -6,13 +6,47 @@
 # import google_trans_new
 # from google_trans_new import google_translator
 from deep_translator import GoogleTranslator
+import pandas as pd
+from datetime import datetime
 # see available languages with the below
 # print(google_trans_new.LANGUAGES)
 
-# Init
-translator = GoogleTranslator(source='auto', target='fr')
 
-# Call to translate and get output
-output = translator.translate(
-    "I love natural language processing")
-print(output)
+
+def apply_translate(translator, text):
+    output = translator.translate(text)
+
+    return output
+
+def reverse_translate(reverse_translator, text):
+    output = reverse_translator.translate(text)
+    return output
+
+def main():
+    og_df = pd.read_csv('final_data_v2.csv')
+
+    target_lang = "fr"
+    translator = GoogleTranslator(source='auto', target=target_lang)
+    reverse_translator = GoogleTranslator(source=target_lang, target="en")
+
+
+
+    # Apply initial translation.
+    translate_df = og_df.copy()
+    translate_df["sentence"] = translate_df["sentence"].apply(lambda x: None)
+    translate_df["language"] = target_lang
+    print(translate_df.head())
+
+    # Reverse translation for paraphasing. 
+    reverse_translate_df = translate_df.copy()
+    reverse_translate_df["language"] = "sloppy"
+
+    # Output DF
+    output_df = pd.concat([translate_df, reverse_translate_df], axis=0)
+    print(len(output_df))
+    print(output_df.head())
+    print(output_df.tail())
+
+
+if __name__ == "__main__":
+    main()
